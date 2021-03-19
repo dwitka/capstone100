c2a685b2bd8c46fdb7eac027715cfe3682f8b7f5
David <david.witka@humber.ca>
2021-03-18 20:36:39 -0400

    added @app.errorhandler decorators

--- app.py
+++ app.py
@@ -125,6 +125,71 @@ def create_app(test_config=None):
     def be_cool():
         return "Be cool, man, be coooool! You're almost a FSND grad!"
 
+    # Error Handling
+    '''
+    Example error handling for unprocessable entity
+    '''
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+                        "success": False,
+                        "error": 422,
+                        "message": "unprocessable"
+                        }), 422
+
+
+    '''
+    implement error handlers using the @app.errorhandler(error) decorator
+        each error handler should return (with approprate messages):
+                 jsonify({
+                        "success": False,
+                        "error": 404,
+                        "message": "resource not found"
+                        }), 404
+    '''
+    @app.errorhandler(403)
+    def unprocessable(error):
+        return jsonify({
+                        "success": False,
+                        "error": 403,
+                        "message": "don't have permissions"
+                        }), 403
+
+
+    '''
+    implement error handler for 404
+        error handler should conform to general task above
+    '''
+    @app.errorhandler(404)
+    def resource_not_found(error):
+        return jsonify({
+                        "success": False,
+                        "error": 404,
+                        "message": "resource not found"
+                        }), 404
+
+
+    @app.errorhandler(401)
+    def resource_not_found(error):
+        return jsonify({
+                        "success": False,
+                        "error": 401,
+                        "message": "Unauthorized"
+                        }), 401
+
+
+    '''
+    implement error handler for AuthError
+        error handler should conform to general task above
+    '''
+    @app.errorhandler(AuthError)
+    def autherror(error):
+        return jsonify({
+                        "success": False,
+                        "error": AuthError,
+                        "message": "AuthError"
+                        }), AuthError
+
     return app
 
 app = create_app()
