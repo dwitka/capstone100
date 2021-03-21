8f9f3b4ca1e97e5561935c42115cd46407ce0c85
David <david.witka@humber.ca>
2021-03-21 14:41:39 -0400

    switched to remote database

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
-# database_path = os.environ['DATABASE_URL']
-database_path = 'postgresql://postgres:postGres44@localhost:5432/trivia'
+database_path = os.environ['DATABASE_URL']
+# database_path = 'postgresql://postgres:postGres44@localhost:5432/trivia'
 db = SQLAlchemy()
 
 '''
