f2bf6e641f72f9000eef7cbedbc1b3320f6a29dc
David <david.witka@humber.ca>
2021-03-29 11:35:47 -0400

    added method render_page() to app.py

--- app.py
+++ app.py
@@ -43,13 +43,19 @@ def create_app(test_config=None):
         return render_template('login.html')
 
 
+    @app.route('/render_page', methods=['GET', 'POST'])
+    def render_page():
+        return render_template('set_jwt.html')
+    
+
     @app.route('/set_jwt', methods=['GET', 'POST'])
     def set_jwt():
+        render_page()
         data = request.get_json()
         print(data)
         permissions = data['permissions']
         os.environ['PERMISSIONS'] = permissions
-        return render_template('set_jwt.html')
+        return redirect(url_for('movies')
 
 
     @app.route('/movies', methods=['GET'])
