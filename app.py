29bec39e45502abeaa88c30bb361a7db7087f670
David <david.witka@humber.ca>
2021-04-04 19:10:59 -0400

    making changes

--- app.py
+++ app.py
@@ -1,239 +1,226 @@
 import os
-from flask import Flask, jsonify, render_template, request, abort, redirect, url_for
-from models import setup_db, Movie, Actor
-from flask_cors import CORS
+from flask import Flask, render_template, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from sqlalchemy import exc
-from auth.auth import AuthError, requires_auth
+from auth import AuthError, requires_auth
+from models import db_drop_and_create_all, setup_db, Movie, Actor
 
-'''
-Endpoints:
-
-    GET /actors and /movies
-    DELETE /actors/ and /movies/
-    POST /actors and /movies and
-    PATCH /actors/ and /movies/
-    '''
 
 def create_app(test_config=None):
+    # create and configure the app
     app = Flask(__name__)
+    app.secret_key = 'SECRET'
     setup_db(app)
     CORS(app, resources={"/": {"origins": "*"}})
-    
+
     @app.after_request
     def after_request(response):
-        '''response.headers.add(
-            'Access-Control-Allow-Origin', 'https://capstone100.herokuapp.com')'''
-        response.headers.add(
-            'Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
-        response.headers.add(
-            'Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type, Authorization, true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET, PATCH, PUT, POST,DELETE, OPTIONS')
         return response
 
+    db_drop_and_create_all()
 
+    # Home
     @app.route('/')
     def index():
-        return redirect(url_for('login')), 200
+        return render_template('index.html')
 
-
-    '''login page should route to Auth0 site and Authorize user.'''
-    @app.route('/login', methods=['GET'])
-    def login():
-        return render_template('login.html'), 200
-    
-
+    # Movie endpoints
     @app.route('/movies', methods=['GET'])
     @requires_auth('get:movies')
     def get_movies(payload):
-        '''returns a list of movies'''
         try:
             movies = Movie.query.all()
-            movies_list = []
-            count = 0
-            if len(movies) != 0:
-                while count < len(movies):
-                    movies_list.append(movies[count].format())
-                    count = count + 1
-            else:
-                pass
+            return jsonify({
+                'success': True,
+                'movies': [movie.format() for movie in movies]
+                }), 200
         except Exception:
             abort(500)
-        return jsonify({
-                'success': True,
-                'movies': movie.format()
-                }), 200
-
 
     @app.route('/movies', methods=['POST'])
     @requires_auth('post:movies')
-    def add_movies(payload):
-        '''creates a new row in the movies table'''
-        
-        data = request.json_get()
-        movie = Movie(title=data['title'],release_date=data['release_date'])
+    def add_movie(payload):
+        data = request.get_json()
+        movie = Movie(
+            title=data['title'],
+            release_date=data['release_date']
+            )
+        if movie.title == '' or movie.release_date == '':
+            abort(422)
+
         try:
             movie.insert()
-            return jsonify({"success": True,
-                        "movies": [movie.format()]}), 200
+
+            return jsonify({
+                'success': True,
+                'movie': movie.format()
+                }), 200
         except Exception:
             abort(500)
 
-    @app.route('/movies/<edit_id>', methods=['PATCH'])
+    @app.route('/movies/<int:id>', methods=['GET', 'PATCH'])
     @requires_auth('patch:movies')
-    def edit_movies(payload, edit_id):
-        '''changes the name of the movie'''
-        movie = Movie.query.get(edit_id)
-        if not movie:
-            abort(401)
+    def update_movie(payload, id):
+        new_info = request.get_json()
+        movie = Movie.query.filter(Movie.id == id).one_or_none()
+        if movie:
+            movie.title = (new_info['title'] if new_info['title']
+                           else movie.title)
+            movie.release_date = (new_info['release_date'] if
+                                  new_info['release_date'] else
+                                  movie.release_date)
         else:
-            try:
-                new_title = request.form.get('edit_title')
-                new_release_date = request.form.get('edit_release_date')
-                movie.release_date = new_release_date
-                movie.title = new_title
-                movie.update()
-            except Exception:
-                abort(422)
-        return jsonify({"success": True, "movies": [movie.format()]}), 200
+            abort(404)
+        try:
+            movie.update()
 
+            return jsonify({
+                'success': True,
+                'movie': [movie.format()]
+                }), 200
+        except Exception:
+            abort(500)
 
-    @app.route('/movies/<movie_id>/deleted', methods=['DELETE'])
+    @app.route('/movies/<int:id>', methods=['DELETE'])
     @requires_auth('delete:movies')
-    def delete_movies(payload, movie_id):
-        '''deletes a movie'''
-        movie = Movie.query.get(movie_id)
-        if not movie:
-            abort(401)
+    def delete_movie(payload, id):
+        movie = Movie.query.filter(Movie.id == id).one_or_none()
+        if movie:
+            try:
+                movie.delete()
+                return jsonify({
+                    'success': True,
+                    'delete': id
+                    }), 200
+            except Exception:
+                db.session.rollback()
+                abort(500)
         else:
-            movie.delete()
-            return jsonify({"success": True, "delete": movie_id}), 200
+            abort(404)
 
-
+    # Actor endpoints
     @app.route('/actors', methods=['GET'])
     @requires_auth('get:actors')
-    def get_actors():
-        '''returns a list of actors'''
+    def get_actors(payload):
         try:
             actors = Actor.query.all()
-            actors_list = []
-            count = 0
-            if len(actors) != 0:
-                while count < len(actors):
-                    actors_list.append(actors[count].format())
-                    count = count + 1
-            else:
-                pass
+            return jsonify({
+                            'success': True,
+                            'actors': [actor.format() for actor in actors]
+                            }), 200
         except Exception:
             abort(500)
-        return render_template('actors.html', actors=actors_list, list_header="Actors!"), 200
-
 
     @app.route('/actors', methods=['POST'])
     @requires_auth('post:actors')
-    def add_actors(payload):
-        '''creates a new row in the actors table'''
-        data_n = request.form.get('name')
-        if data_n:
-            actor = Actor(name=data_n, age=00, gender="")
+    def add_actor(payload):
+        data = request.get_json()
+        actor = Actor(
+            name=data['name'],
+            age=data['age'],
+            gender=data['gender'],
+            movie_id=data['movie_id']
+            )
+        if actor.name == '' or actor.age == '' or actor.gender == '':
+            abort(422)
+        try:
             actor.insert()
-        else:
-            abort(403)
-        return jsonify({"success": True,
-                        "actors": [actor.format()]}), 200
 
+            return jsonify({
+                'success': True,
+                'actor': actor.format()
+                }), 200
+        except Exception:
+            abort(500)
 
-    @app.route('/actors/<edit_id>', methods=['PATCH'])
+    @app.route('/actors/<int:id>', methods=['GET', 'PATCH'])
     @requires_auth('patch:actors')
-    def edit_actors(payload, edit_id):
-        '''changes the name of the actor'''
-        actor = Actor.query.get(edit_id)
-        if not actor:
-            abort(401)
-        else:
+    def update_actor(payload, id):
+        new_info = request.get_json()
+        actor = Actor.query.filter(Actor.id == id).one_or_none()
+        if actor:
+            actor.name = (new_info['name'] if new_info['name']
+                          else Actor.name)
+            actor.age = (new_info['age'] if new_info['age']
+                         else actor.age)
+            actor.gender = (new_info['gender'] if new_info['gender']
+                            else actor.gender)
+            actor.movie_id = (new_info['movie_id'] if new_info['movie_id']
+                              else actor.movie_id)
             try:
-                new_name = request.form.get('edit_name')
-                new_age = request.form.get('edit_age')
-                actor.name = new_name
-                actor.age = new_age
                 actor.update()
+                return jsonify({
+                    'success': True,
+                    'actor': [actor.format()]
+                    }), 200
             except Exception:
-                abort(422)
-        return jsonify({"success": True, "actors": [actor.format()]}), 200
-
-
-    @app.route('/actors/<actor_id>/deleted', methods=['DELETE'])
-    @requires_auth('delete:actors')
-    def delete_actors(payload, actor_id):
-        '''deletes an actor'''
-        actor = Actor.query.get(actor_id)
-        if not actor:
-            abort(401)
+                abort(500)
         else:
-            actor.delete()
-            return jsonify({"success": True, "delete": actor_id}), 200
+            abort(404)
 
-
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+    @app.route('/actors/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:actors')
+    def delete_actor(payload, id):
+        actor = Actor.query.filter(Actor.id == id).one_or_none()
+        if actor:
+            try:
+                actor.delete()
+                return jsonify({
+                    'success': True,
+                    'delete': id
+                    }), 200
+            except Exception:
+                db.session.rollback()
+                abort(500)
+        else:
+            abort(404)
 
     # Error Handling
-    '''
-    Example error handling for unprocessable entity
-    '''
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad Request, please check your inputs"
+            }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found."
+            }), 404
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Sorry, there's a problem on our end."
+            }), 500
+
     @app.errorhandler(422)
     def unprocessable(error):
         return jsonify({
-                        "success": False,
-                        "error": 422,
-                        "message": "unprocessable"
-                        }), 422
+            'success': False,
+            'error': 422,
+            'message': 'unprocessable. Check your input.'
+          }), 422
 
-
-    '''
-    implement error handlers using the @app.errorhandler(error) decorator
-        each error handler should return (with approprate messages):
-                 jsonify({
-                        "success": False,
-                        "error": 404,
-                        "message": "resource not found"
-                        }), 404
-    '''
     @app.errorhandler(403)
-    def unprocessable(error):
+    def forbidden(error):
         return jsonify({
-                        "success": False,
-                        "error": 403,
-                        "message": "don't have permissions"
-                        }), 403
+            "success": False,
+            "error": 403,
+            "message": "You are not allowed to access this resource",
+                }), 403
 
-
-    '''
-    implement error handler for 404
-        error handler should conform to general task above
-    '''
-    @app.errorhandler(404)
-    def resource_not_found(error):
-        return jsonify({
-                        "success": False,
-                        "error": 404,
-                        "message": "resource not found"
-                        }), 404
-
-
-    @app.errorhandler(401)
-    def resource_not_found(error):
-        return jsonify({
-                        "success": False,
-                        "error": 401,
-                        "message": "Unauthorized"
-                        }), 401
-
-
-    '''
-    implement error handler for AuthError
-        error handler should conform to general task above
-    '''
     @app.errorhandler(AuthError)
     def auth_error(ex):
         res = jsonify(ex.error)
@@ -242,7 +229,9 @@ def create_app(test_config=None):
 
     return app
 
-app = create_app()
+
+APP = create_app()
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,85 +1,75 @@
-from sqlalchemy import Column, Integer, String, create_engine
-from flask_sqlalchemy import SQLAlchemy
-import json
 import os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, DateTime
+import json
 
-#database_path = os.environ['DATABASE_URL']
+#database_path = os.getenv("DATABASE_URL")
 database_path = 'postgresql://postgres:postGres44@localhost:5432/casting'
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     db.app = app
     db.init_app(app)
+
+
+def db_drop_and_create_all():
+    db.drop_all()
     db.create_all()
 
 
-'''
-Person
-Have title and release year
-'''
-class Actor(db.Model):
-    __tablename__ = 'Actors'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    age = db.Column(db.Integer)
-    gender = db.Column(db.String)
-
-    def __init__(self, name, age=00, gender=""):
-        self.name = name
-        self.age = age
-        self.gender = gender
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    release_date = Column(DateTime(), nullable=False)
+    actors = db.relationship('Actor', cascade="all, delete", backref='movies')
 
     def format(self):
-        return {
+        return{
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String, nullable=False)
+    movie_id = Column(Integer, db.ForeignKey('movies.id'), nullable=False)
+
+    def format(self):
+        return{
             'id': self.id,
             'name': self.name,
             'age': self.age,
-            'gender': self.gender}
+            'gender': self.gender,
+            'movie_id': self.movie_id
+        }
 
     def insert(self):
         db.session.add(self)
         db.session.commit()
 
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-
     def update(self):
-        db.session.commit()
-
-class Movie(db.Model):  
-    __tablename__ = 'Movies'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    release_date = db.Column(db.String)
-
-    def __init__(self, title, release_date=""):
-        self.title = title
-        self.release_date = release_date
-
-    def format(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'release_date': self.release_date}
-
-    def insert(self):
-        db.session.add(self)
         db.session.commit()
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-
-    def update(self):
-        db.session.commit()
-
-
